18f1908790a6f8ec6910789e9b3b92682cab41ad
toffa harryson <toffaharryson3@gmail.com>
2026-07-21 16:08:39 +0200

    modification fichier view_connexion.php

---
+++ view/view_connexion.php
@@ -0,0 +1,18 @@
+<html lang="fr">
+<head>
+  <meta https-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=devie-width, initial-scale=1.0">
+  <link rel="stylsheet" href="./style/style.css">
+  <script src="./script/script.js" defer></script>
+  <title>Connexion</title>
+</head>
+<body>
+  <h1>connexion : </h1>
+  <form action="" method="post">
+    <p>Veuillez saisir votre identifiant :</p>
+    <input type="text" name="login>
+    <p>veuillez saisir votre mot de passe :</p>
+    <input type="submit" value="connexion">
+  </form>
+</body>
+</html>
